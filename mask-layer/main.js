2feb3a29673a1c6201cb4cdc1462106f6714d446
92sagarchougule <92chougulesagar@gmail.com>
2023-06-19 11:16:17 +0530

    code for masking layer

---
+++ mask-layer/main.js
@@ -0,0 +1,90 @@
+import GeoJSON from 'ol/format/GeoJSON.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {Fill, Style} from 'ol/style.js';
+import {OSM, Stamen, Vector as VectorSource} from 'ol/source.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {fromLonLat} from 'ol/proj.js';
+import {getVectorContext} from 'ol/render.js';
+import XYZ from 'ol/source/XYZ.js';
+import { transform } from 'ol/proj';
+
+
+
+
+
+
+
+
+//A distinct className is required to use another canvas for the background
+
+const key = "8H3vz4wO6mZAX9q3EEdG"
+
+const background =new TileLayer({
+  className: 'ol-layer-imagery',
+  source: new XYZ({
+    //attributions:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ',
+    url: 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + key,
+    //maxZoom: 20,
+    //crossOrigin: '',
+  }),
+});
+
+
+// const background = new TileLayer({
+//   className: 'stamen',
+//   source: new Stamen({
+//     layer: 'toner',
+//   }),
+// });
+
+const base = new TileLayer({
+  source: new OSM(),
+});
+
+const clipLayer = new VectorLayer({
+  style: null,
+  source: new VectorSource({
+    url: './data/map.geojson',
+    format: new GeoJSON(),
+  }),
+});
+
+//Giving the clipped layer an extent is necessary to avoid rendering when the feature is outside the viewport
+clipLayer.getSource().on('addfeature', function () {
+  background.setExtent(clipLayer.getSource().getExtent());
+});
+
+const style = new Style({
+  fill: new Fill({
+    color: 'black',
+  }),
+});
+
+background.on('postrender', function (e) {
+  const vectorContext = getVectorContext(e);
+  e.context.globalCompositeOperation = 'destination-in';
+  clipLayer.getSource().forEachFeature(function (feature) {
+    vectorContext.drawFeature(feature, style);
+  });
+  e.context.globalCompositeOperation = 'source-over';
+});
+
+const map = new Map({
+  layers: [ base, background, clipLayer], //background,
+  target: 'map',
+  view: new View({
+    center: fromLonLat([75.4924070208232, 19.631953861800696]),
+    zoom: 7,
+  }),
+});
+
+
+
+map.on('click', function(evt){
+  var lonlat = transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+  console.log(lonlat)
+  var lon = lonlat[0];
+  var lat = lonlat[1];
+  //alert("You clicked near lat lon: "+ lon.toFixed(6) + "  " + lat.toFixed(6));  // 831564965234.545288, 16.441194
+});
